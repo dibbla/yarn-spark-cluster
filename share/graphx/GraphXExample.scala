9c32092bf3480b2bca6895d86a7689de90d3d972
dibbla <120090375@link.cuhk.edu.cn>
2025-03-16 12:14:47 +0800

    add connected comp

--- share/graphx/GraphXExample.scala
+++ share/graphx/GraphXExample.scala
@@ -14,21 +14,21 @@ object GraphXExampleHDFS {
     // Define HDFS path for the input edge file.
     val edgesPath = "hdfs://master:9000/edge.txt"      // Adjust this HDFS path as needed.
     // Define the output directory on HDFS
-    val outputPath = "hdfs://master:9000/pagerank_output" // Make sure this directory doesn't exist before running
+    val outputPath = "hdfs://master:9000/connected_components_output" // Make sure this directory doesn't exist before running
 
     // Load the graph from the HDFS edge list file.
     val graph = GraphLoader.edgeListFile(sc, edgesPath)
 
-    // Run the PageRank algorithm on the graph.
-    val ranks = graph.staticPageRank(10).vertices
+    // Find the connected components
+    val cc = graph.connectedComponents().vertices
 
     // Dump the results to HDFS.
-    ranks.saveAsTextFile(outputPath)
+    cc.saveAsTextFile(outputPath)
 
     // Optionally, print some of the results for verification.
-    println("PageRank Results (sample):")
-    ranks.take(10).foreach { case (id, rank) =>
-      println(s"Vertex $id: $rank")
+    println("Connected Components Results (sample):")
+    cc.take(10).foreach { case (id, cc) =>
+      println(s"Vertex $id: $cc")
     }
     
     spark.stop()
--- share/graphx/run_example.sh
+++ share/graphx/run_example.sh
@@ -1,15 +1,55 @@
-hdfs dfs -rm /edge.txt
-hdfs dfs -rm /vertices.txt
-hdfs dfs -put edge.txt /
-hdfs dfs -put vertices.txt /
+# Do facebook
+cp datasets/facebook.txt edge.txt
 
-hdfs dfs -rm -r /pagerank_output
+hdfs dfs -rm /edge.txt
+# hdfs dfs -rm /vertices.txt
+hdfs dfs -put edge.txt /
+# hdfs dfs -put vertices.txt /
+
+hdfs dfs -rm -r /connected_components_output
 
 time spark-submit --master yarn \
     --deploy-mode cluster \
     --name "Graph" \
     --executor-memory 1g \
+    --num-executors 3 \
     --class GraphXExampleHDFS \
     GraphXExample.jar
 
-hdfs dfs -get /pagerank_output /opt/share/graphx/pagerank_output
+hdfs dfs -get /connected_components_output /opt/share/graphx/connected_components_output
+
+# Do epinions
+cp datasets/epinions.txt edge.txt
+
+hdfs dfs -rm /edge.txt
+hdfs dfs -put edge.txt /
+
+hdfs dfs -rm -r /connected_components_output
+
+
+time spark-submit --master yarn \
+    --deploy-mode cluster \
+    --name "Graph" \
+    --executor-memory 1g \
+    --num-executors 3 \
+    --class GraphXExampleHDFS \
+    GraphXExample.jar
+
+hdfs dfs -get /connected_components_output /opt/share/graphx/connected_components_output
+
+# Do google
+cp datasets/google.txt edge.txt
+hdfs dfs -rm /edge.txt
+hdfs dfs -put edge.txt /
+
+hdfs dfs -rm -r /connected_components_output
+
+time spark-submit --master yarn \
+    --deploy-mode cluster \
+    --name "Graph" \
+    --executor-memory 1g \
+    --num-executors 3 \
+    --class GraphXExampleHDFS \
+    GraphXExample.jar
+
+hdfs dfs -get /connected_components_output /opt/share/graphx/connected_components_output
